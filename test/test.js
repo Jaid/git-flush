eadf0d6831bc0e312764f8e88aa6f4599fc83999
Jaid <jaid.jsx@gmail.com>
2019-12-01 14:46:09 +0100

    Fixed usage of `@actions/exec`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ import path from "path"
 import simpleGit from "simple-git/promise"
 import fsp from "@absolunet/fsp"
 import ms from "ms.macro"
-import exec from "@actions/exec"
+import {exec} from "@actions/exec"
 
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
 
