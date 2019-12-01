807a546572ac344b7ea1df58444ffbe2111f1dee
Jaid <jaid.jsx@gmail.com>
2019-12-01 13:29:45 +0100

    add
    --all
    
    --message
    
    E:\Projects\git-flush

--- test/test.js
+++ test/test.js
@@ -1,16 +1,18 @@
 import path from "path"
 
 import simpleGit from "simple-git/promise"
+import fsp from "@absolunet/fsp"
 
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
 
 /**
-   * @type { import("../src") }
-   */
+ * @type { import("../src") }
+ */
 const {default: gitFlush} = indexModule
 
 it("should run", async () => {
   const directory = path.join(__dirname, "..", "dist", "test", "repo")
+  await fsp.mkdirp(directory)
   const gitRepository = simpleGit(simpleGit)
   const result = await gitFlush(path.join(__dirname, ".."))
 })
